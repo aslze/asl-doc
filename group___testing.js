f34f12927fab70032114aeb8c5e98eb3b9353907
aslze <runner@fv-az736-765.alpbqrzxv30uzkvtn2qktnuusd.cx.internal.cloudapp.net>
2024-03-24 00:10:05 +0000

    Update documentation

--- group___testing.js
+++ group___testing.js
@@ -1,8 +1,8 @@
 var group___testing =
 [
-    [ "ASL_APPROX", "group___testing.html#ga2edf56724877b69c5c4fc0b18f9fb961", null ],
     [ "ASL_ASSERT", "group___testing.html#ga1f2c851878c4f646003836416b72495b", null ],
-    [ "ASL_CHECK", "group___testing.html#ga7e93129c77854e8b426678f03455e695", null ],
+    [ "ASL_EXPECT", "group___testing.html#ga57e0883ab245c843c0887226d0558c47", null ],
+    [ "ASL_EXPECT_NEAR", "group___testing.html#ga6a50cd6dfd773af5422552911b504f44", null ],
     [ "ASL_TEST", "group___testing.html#gac5774b842c78f62ab84ac8ee01ef564e", null ],
     [ "ASL_TEST_ENABLE", "group___testing.html#ga30cd7128004a0a458072ac4e20b6cc17", null ],
     [ "ASL_TEST_ENABLE_MAIN", "group___testing.html#gae53f514c21bff75a87682f7975df897c", null ],
